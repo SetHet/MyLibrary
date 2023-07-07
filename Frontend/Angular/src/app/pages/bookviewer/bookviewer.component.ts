fcb816f415f3b6e44a7c776ee1e90c9bc90f25a7
SetHet <sethet1@gmail.com>
2023-07-06 20:45:08 -0400

    Angular, css/html list

--- Frontend/Angular/src/app/pages/bookviewer/bookviewer.component.ts
+++ Frontend/Angular/src/app/pages/bookviewer/bookviewer.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-bookviewer',
   templateUrl: './bookviewer.component.html',
   styleUrls: ['./bookviewer.component.scss']
 })
-export class BookviewerComponent {
+export class BookviewerComponent implements OnInit{
+  constructor(
+    private route: ActivatedRoute,
+  ){}
 
+  book_code: string = '';
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe(params => {
+      this.book_code = params.get('code') || '';
+    })
+  }
 }
--- Frontend/Angular/src/app/pages/mybooks/mybooks.component.ts
+++ Frontend/Angular/src/app/pages/mybooks/mybooks.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mybooks',
   templateUrl: './mybooks.component.html',
   styleUrls: ['./mybooks.component.scss']
 })
-export class MybooksComponent {
+export class MybooksComponent implements OnInit {
+  books_list = [
+    {
+      name: "Libro 1",
+      code: "11111",
+      img_url: "../../../assets/media/imagen-test.png",
+    },
+    {
+      name: "Libro 2",
+      code: "22222",
+      img_url: "../../../assets/media/imagen-test.png",
+    },
+    {
+      name: "Libro 3",
+      code: "33333",
+      img_url: "../../../assets/media/imagen-test.png",
+    }
+  ];
 
+  ngOnInit(): void {
+    // Test
+    // for (let index = 0; index < 2; index++) {
+    //   this.books_list = this.books_list.concat(this.books_list);
+    // }
+  }
+
+  ClickCard(code: string){
+    // cambiar nombre pagina
+    window.document.title = code; 
+
+    // cambiar pagina
+    window.location.href = '/bookviewer?code='+code; 
+    
+  }
 }
