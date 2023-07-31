bb0969b955d5016cb57f7f1711ee9d0d46280c28
SetHet <sethet1@gmail.com>
2023-07-30 22:05:39 -0400

    Update test.js

--- Backend/NodeJS/src/api/test/test.js
+++ Backend/NodeJS/src/api/test/test.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
+module.exports = router
+router.get('/test', test)
 
 function test (req, res) {
   res.send('test page')
 }
-
-router.get('/test', test)
-
-module.exports = router
